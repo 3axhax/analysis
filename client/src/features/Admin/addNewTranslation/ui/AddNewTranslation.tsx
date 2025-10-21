ac450336f4321fdabaf5f08ffb81f564fc52ac9c
Захаров Михаил <zakharov@compel.ru>
2025-10-21 18:53:46 +0500

    Add Admin TranslationPage List Blank Modal

--- client/src/features/Admin/addNewTranslation/ui/AddNewTranslation.tsx
+++ client/src/features/Admin/addNewTranslation/ui/AddNewTranslation.tsx
@@ -1,4 +1,6 @@
 import { useTranslation } from "react-i18next";
+import { AddNewTranslationModal } from "@features/Admin/addNewTranslation/ui/AddNewTranslationModal.tsx";
+import { useState } from "react";
 
 interface AddNewTranslationProps {
   className?: string;
@@ -6,11 +8,18 @@ interface AddNewTranslationProps {
 
 export const AddNewTranslation = ({ className }: AddNewTranslationProps) => {
   const { t } = useTranslation("features");
+  const [openModal, setOpenModal] = useState<boolean>(false);
   return (
-    <button
-      className={`bg-green-600 text-white rounded-lg px-4 py-2 cursor-pointer hover:bg-green-500${className ? " " + className : ""}`}
-    >
-      {t("addNewTranslation.add")}
-    </button>
+      <>
+        <button
+            className={`bg-green-600 text-white rounded-lg px-4 py-2 cursor-pointer hover:bg-green-500${className ? " " + className : ""}`}
+            onClick={() => {
+              setOpenModal(true);
+            }}
+        >
+          {t("addNewTranslation.add")}
+        </button>
+        <AddNewTranslationModal open={openModal} setOpen={setOpenModal} />
+      </>
   );
 };
