cc00f2e2f478c28a963f818e11a02da5d0f918a1
ZakharovaTatiana <scriptanya@gmail.com>
2026-02-11 17:52:32 +0500

    UI // tiny fixing

--- client/src/widgets/navigation/ui/NavigationUI.tsx
+++ client/src/widgets/navigation/ui/NavigationUI.tsx
@@ -23,7 +23,7 @@ export const NavigationUI = ({ navItems }: { navItems: NavItem[] }) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
 
   return (
-    <header className="header bg-cyan-600 shadow-lg dark:bg-gray-950/95 dark:shadow-white/20">
+    <header className="header bg-cyan-600 shadow-lg dark:bg-cyan-950 dark:shadow-white/20">
       <Logo />
       <BurgerButton
         isOpen={isMobileMenuOpen}
@@ -44,7 +44,7 @@ export const NavigationUI = ({ navItems }: { navItems: NavItem[] }) => {
           />
         </div>
         <ul
-          className={`bg-cyan-600 py-4 lg:py-0 flex flex-col lg:justify-start lg:flex-row lg:inline-flex z-40 lg:bg-transparent dark:bg-gray-950 fixed inset-y-0 right-0 w-80 transition-transform duration-300 transform lg:relative lg:right-auto lg:translate-x-0 lg:w-auto lg:px-0 ${isMobileMenuOpen ? "translate-x-0" : "translate-x-full"}`}
+          className={`bg-cyan-600 dark:bg-cyan-950 py-4 lg:py-0 flex flex-col lg:justify-start lg:flex-row lg:inline-flex z-40 lg:bg-transparent fixed inset-y-0 right-0 w-80 transition-transform duration-300 transform lg:relative lg:right-auto lg:translate-x-0 lg:w-auto lg:px-0 ${isMobileMenuOpen ? "translate-x-0" : "translate-x-full"}`}
         >
           {navItems.map((item) => (
             <li key={item.key} className={"flex justify-stretch w-full m-0"}>
@@ -60,7 +60,7 @@ export const NavigationUI = ({ navItems }: { navItems: NavItem[] }) => {
                   >
                     <span
                       className={
-                        "hover:cursor-pointer w-full transition-colors duration-200 group text-orange-50 hover:text-orange-300 hover:border-orange-300 py-2 lg:py-6 px-3 flex items-center bg-cyan-600 lg:w-50 z-2"
+                        "hover:cursor-pointer w-full transition-colors duration-200 group text-orange-50 hover:text-orange-300 hover:border-orange-300 py-2 lg:py-6 px-3 flex items-center bg-cyan-600 dark:bg-cyan-950 lg:w-50 z-2"
                       }
                     >
                       {item.iconLink && item.iconLink}
@@ -77,7 +77,7 @@ export const NavigationUI = ({ navItems }: { navItems: NavItem[] }) => {
                     </span>
                     <div
                       className={`relative overflow-hidden lg:absolute flex flex-col gap-2 lg:left-0 w-full lg:w-48 lg:rounded-b-lg 
-                               bg-orange-400/90 lg:shadow-lg shadow-cyan-950/40 transition-all duration-300 ease-linear ${isDropdownOpen === item.key ? "max-h-[500px] lg:translate-y-0 z-1 lg:top-full" : "max-h-0 lg:-translate-y-full z-0 lg:-top-full"}`}
+                               bg-orange-400/90 dark:bg-orange-700/90 lg:shadow-lg shadow-cyan-950/40 transition-all duration-300 ease-linear ${isDropdownOpen === item.key ? "max-h-[500px] lg:translate-y-0 z-1 lg:top-full" : "max-h-0 lg:-translate-y-full z-0 lg:-top-full"}`}
                     >
                       <ul className={"py-3 flex flex-col w-full m-0"}>
                         {item.items?.map(
@@ -112,7 +112,7 @@ export const NavigationUI = ({ navItems }: { navItems: NavItem[] }) => {
               ) : item.isButton ? (
                 <button
                   className={
-                    "cursor-pointer flex justify-center items-center text-white transition-colors lg:mr-6"
+                    "cursor-pointer flex justify-center items-center text-white transition-colors ml-3 lg:ml-0 lg:mr-6"
                   }
                   key={item.key}
                   onClick={item.onClick}
@@ -124,7 +124,7 @@ export const NavigationUI = ({ navItems }: { navItems: NavItem[] }) => {
                 <a
                   href={"/#" + item.key}
                   className={`relative flex w-full overflow-hidden items-center hover:text-shadow-[0_0px_.5px_#faa968] 
-                          flex-gap-2 pr-10 pl-3 lg:pl-0 py-2 lg:py-6 bg-cyan-600 z-2 uppercase group lg:whitespace-pre hover:text-orange-50  ${
+                          flex-gap-2 pr-10 pl-3 lg:pl-0 py-2 lg:py-6 bg-cyan-600 dark:bg-cyan-950 z-2 uppercase group lg:whitespace-pre hover:text-orange-50  ${
                             location.pathname === item.path
                               ? " text-orange-200"
                               : " text-orange-50"
