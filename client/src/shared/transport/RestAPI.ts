dad3f2fb377fb15f0abe29d1b3d00026baa4cbc1
Захаров Михаил <zakharov@compel.ru>
2026-03-20 17:28:28 +0500

    CORS Fix

--- client/src/shared/transport/RestAPI.ts
+++ client/src/shared/transport/RestAPI.ts
@@ -68,7 +68,13 @@ class RestAPI {
       data: object;
       headers?: object;
     } = {
-      url: this.baseUrl + this.target,
+      //url: this.baseUrl + this.target,
+      url:
+          window.location.protocol +
+          "//" +
+          window.location.hostname +
+          ":5050" +
+          this.target,
       method: this.method,
       data: this.data,
     };
