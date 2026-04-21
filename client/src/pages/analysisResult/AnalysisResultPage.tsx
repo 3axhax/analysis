3ca1e3ecb6007e86ea34805f9ee9522c6cfaed62
ZakharovaTatiana <scriptanya@gmail.com>
2026-04-21 22:55:22 +0500

    result table  // copy link

--- client/src/pages/analysisResult/AnalysisResultPage.tsx
+++ client/src/pages/analysisResult/AnalysisResultPage.tsx
@@ -32,30 +32,24 @@ export const AnalysisResultPage = () => {
 
   return (
     <div className="app">
-      <div className="mb-4">
-        <h1 className="text-4xl font-light text-gray-900 dark:text-white mb-4">
-          {title}
-          <button type={'button'} onClick={handleCopy} title="Скопировать ссылку на результатврасшифровки ${resultId}" className={'size-6 cursor-pointer ml-2'}>
-            {copied ? (
-                <CheckIcon className="size-6 inline-flex mx-1 text-green-600" />
-            ) : (
-                <LinkIcon className={'size-6 inline-flex mx-1 text-gray-500 hover:text-gray-700 transition-colors'} />
-            )}
-          </button>
-        </h1>
-      </div>
+      <h1 className="text-4xl font-light text-gray-900 dark:text-white mb-4">
+        {title}
+        <button type={'button'} onClick={handleCopy} title="Скопировать ссылку на результатврасшифровки ${resultId}" className={'size-6 cursor-pointer ml-2'}>
+          {copied ? (
+              <CheckIcon className="size-6 inline-flex mx-1 text-green-600" />
+          ) : (
+              <LinkIcon className={'size-6 inline-flex mx-1 text-gray-500 hover:text-gray-700 transition-colors'} />
+          )}
+        </button>
+      </h1>
       {analysisResult ? (
         <div className="mx-auto text-left">
           <div className="bg-white dark:bg-blue-950 rounded-xl p-8 shadow-lg">
             <div className={"flex gap-3 mb-2 border-b-1 border-gray-300"}>
-              <div>
-                {t("gender")}:{" "}
+              <span className={'text-gray-600'}>{t("gender")}:{" "}</span>
                 {tEntities(`gender.${analysisResult.result.Gender.name}`)}
-              </div>
-              <div>
-                {t("age")}:{" "}
-                {tEntities(`ages.${analysisResult.result.Age.name}`)}
-              </div>
+                <span className={'text-gray-600'}>{t("age")}:{" "}</span>
+                  {tEntities(`ages.${analysisResult.result.Age.name}`)}
             </div>
             {analysisResult.descriptions.length > 0 && (
               <AnalysisDescriptionList
